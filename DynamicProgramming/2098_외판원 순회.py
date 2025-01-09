c59f5f687883fc08207735c3d21de17a90f6fee2
김명준 <86913355+mjj111@users.noreply.github.com>
2025-01-09 20:36:08 +0900

    2025.01.09 문제 해결

--- DynamicProgramming/2098_외판원 순회.py
+++ DynamicProgramming/2098_외판원 순회.py
@@ -1,15 +1,21 @@
 import sys
+sys.setrecursionlimit(10**6)
 INF = 16000000
 input = sys.stdin.readline
 
 N = int(input())
 W = [list(map(int, input().split())) for _ in range(N)]
 
+dp = [[-1] * (1 << N) for _ in range(N)]
+
 def dfs(now, visit):
   if visit == (1 << N) - 1:  # 모든 노드를 방문한 경우
-    if W[now][0] > 0 :
-      return W[now][0] #시작점으로 돌아갈 수 있으면 비용 반환
+    if W[now][0] > 0:  # 시작점으로 돌아갈 수 있는 경우
+      return W[now][0]
     return INF
+
+  if dp[now][visit] != -1:  # 이미 계산된 경우
+    return dp[now][visit]
 
   min_cost = INF
   for next_node in range(N):
@@ -19,7 +25,7 @@ def dfs(now, visit):
     cost = dfs(next_node, next_visited) + W[now][next_node]
     min_cost = min(min_cost, cost)
 
+  dp[now][visit] = min_cost
   return min_cost
 
 print(dfs(0, 1))
-
